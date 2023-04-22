37adb8ed10e1cadbdd774518567ade87c4e2f31f
Michelle Glinkowski <miglinkowski@aol.com>
2023-04-21 22:09:35 -0500

    creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+const headquarters = 42
+function distanceFromHqInBlocks(blocks) {
+    return Math.abs(blocks - headquarters);
+}
+function distanceFromHqInFeet(blocks) {
+    return (distanceFromHqInBlocks(blocks) * 264);
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination-start)* 264;
+}
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet (start,destination);
+    if (distance<= 400) {
+        return 0;
+    }
+    if (distance >= 400 && distance <= 2000) {
+        return (distance-400)* 0.02;
+    }
+    else if (distance >= 2000 && distance <=2500) {
+        return 25;
+    }
+    else if (distance> 2500){
+        return "cannot travel that far";
+    }
+}
